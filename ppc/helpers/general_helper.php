cd15416779aebc0044b2976b04cd493cc57cde99
Muhammad Abubaker <abmasood5900@gmail.com>
2025-06-30 18:57:17 +0500

    minor changes

--- ppc/helpers/general_helper.php
+++ ppc/helpers/general_helper.php
@@ -1117,7 +1117,7 @@ function fb_page_fetch_more_posts($url, $page, $userID, $timeslots, $mode)
 {
 	$CI = &get_instance();
 	$CI->load->library('feedFetcher'); // Load our custom library
-	$feed = $CI->feedFetcher->fetchFeedItems($url, 0);
+	$feed = $CI->feedfetcher->fetchFeedItems($url, 0);
 	if ($feed["success"]) {
 		$items = $feed["items"];
 		foreach ($items as $data) {
@@ -1182,7 +1182,7 @@ function pin_board_fetch_more_posts($url, $page, $userID, $timeslots, $mode)
 {
 	$CI = &get_instance();
 	$CI->load->library('feedFetcher'); // Load our custom library
-	$feed = $CI->feedFetcher->fetchFeedItems($url, 0);
+	$feed = $CI->feedfetcher->fetchFeedItems($url, 0);
 	if ($feed["success"]) {
 		$items = $feed["items"];
 		foreach ($items as $data) {
@@ -1245,7 +1245,7 @@ function ig_user_fetch_more_posts($url, $page, $userID, $timeslots, $mode)
 {
 	$CI = &get_instance();
 	$CI->load->library('feedFetcher'); // Load our custom library
-	$feed = $CI->feedFetcher->fetchFeedItems($url, 0);
+	$feed = $CI->feedfetcher->fetchFeedItems($url, 0);
 	if ($feed["success"]) {
 		$items = $feed["items"];
 		foreach ($items as $data) {
@@ -1644,11 +1644,11 @@ function pin_board_fetch_past_posts($url, $board_id, $user_id, $mode)
 {
 	$CI = &get_instance();
 	$CI->load->library('feedFetcher'); // Load our custom library
-	$pin_board = $CI->Publisher_model->retrieve_record('pinterest_boards', $board_id);
-	$pin_user = $CI->Publisher_model->get_allrecords('pinterest_users', ['user_id' => $user_id]);
-	$CI->Publisher_model->update_last_run($board_id, 'last_run', 'pinterest_boards');
-	$feed = $CI->feedFetcher->fetchSitemapItems($url, 0);
+	$feed = $CI->feedfetcher->fetchSitemapItems($url, 0);
 	if ($feed["success"]) {
+		$pin_board = $CI->Publisher_model->retrieve_record('pinterest_boards', $board_id);
+		$pin_user = $CI->Publisher_model->get_allrecords('pinterest_users', ['user_id' => $user_id]);
+		$CI->Publisher_model->update_last_run($board_id, 'last_run', 'pinterest_boards');
 		$items = $feed["items"];
 		foreach ($items as $data) {
 			// Check if the URL is already in the database
