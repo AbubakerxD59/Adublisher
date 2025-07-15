06212e73b22232e466f671cdb33f69b04e590605
Muhammad Abubaker <abmasood5900@gmail.com>
2025-07-15 18:55:59 +0500

    minor chnages

--- ppc/views/templates/publisher/scripts.php
+++ ppc/views/templates/publisher/scripts.php
@@ -85,7 +85,7 @@
     }
 
     function empty($string) {
-        if ($string == "" || $string == null "" || $string == undefined) {
+        if ($string == "" || $string == null || $string == undefined) {
             return true;
         } else {
             return false;
