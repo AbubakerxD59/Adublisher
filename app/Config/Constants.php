5a0b86c3898bb2abb2461da96c88c8285728ce25
Asadabdullatif <asadabdullatif@cybertronlabs.com>
2025-08-09 12:12:18 +0500

    removed constants

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -131,28 +131,3 @@ define('BulkAssets', BASEURL . 'assets/bulkuploads/'); // database error
 define('AdminAssets', BASEURL . 'assets/admin/'); // database error
 define('PubLandingpageAssets', BASEURL . 'assets/landingpage/');
 define('SERVER_TZ', 'America/Toronto');
-// Test credential
-define('STRIPE_PUBLISHABLE_KEY', 'pk_test_eLIe6vRiQYd6vHoaDKrfrMIK');
-define('STRIPE_SECRET_KEY', 'sk_test_dPLesifQRZgflEOk4yy4x6Ih');
-defined('PINTEREST_CLIENT_ID') || define('PINTEREST_CLIENT_ID', '1523051');
-defined('PINTEREST_CLIENT_SECRET') || define('PINTEREST_CLIENT_SECRET', '7a38aa506baccb9615adcfefd4a015bf6e6616a2');
-// facebook credentials
-defined('FACEBOOK_CLIENT_ID') || define('FACEBOOK_CLIENT_ID', '1262896354847218');
-defined('FACEBOOK_CLIENT_SECRET') || define('FACEBOOK_CLIENT_SECRET', '33c33582e27e4d4fa22f0f677062af1f');
-// instagram credentials
-defined('INSTAGRAM_CLIENT_ID') || define('INSTAGRAM_CLIENT_ID', '1262896354847218');
-defined('INSTAGRAM_CLIENT_SECRET') || define('INSTAGRAM_CLIENT_SECRET', '33c33582e27e4d4fa22f0f677062af1f');
-defined('REVIEW_REQUEST') || define('REVIEW_REQUEST', '');
-// google auth credentials
-defined('GOOGLE_CLIENT_ID') || define('GOOGLE_CLIENT_ID', "548300249090-he2ucj6u2fe0lmr87ek0r5kjqbk37r4o.apps.googleusercontent.com");
-defined('GOOGLE_CLIENT_SECRET') || define('GOOGLE_CLIENT_SECRET', "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");
-// AWS S3 Bucket credentials
-defined('S3_BUCKET') || define('S3_BUCKET', "adublisher-uploads");
-defined('S3_CLIENT_KEY') || define('S3_CLIENT_KEY', "AKIAQ4TWN7DWKHGC4XWP");
-defined('S3_CLIENT_SECRET') || define('S3_CLIENT_SECRET', "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");
-// CALENDARIFIC
-defined('CALENDARIFIC_API_KEY') || define('CALENDARIFIC_API_KEY', "7XbVPY1CdlvbxtigvLHR6EzFF9yGN1Dd");
-// TIKTOK
-defined('TIKTOK_CLIENT_KEY') || define('TIKTOK_CLIENT_KEY', 'awh2hd6t7wkeenh4');
-defined('TIKTOK_CLIENT_SECRET') || define('TIKTOK_CLIENT_SECRET', 'xofpLVwJ4l3CrQQlNDqy2rezxBoECYYo');
-defined('TIKTOK_REDIRECT_URI') || define('TIKTOK_REDIRECT_URI', SITEURL . 'tiktok/redirect');
